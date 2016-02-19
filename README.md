b33f54ef53baf740f5d39c997c51ecdc47a8e4e4
jbousquie <jerome@bousquie.fr>
2016-02-19 17:09:49 +0100

    balistic (to be fixed)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Artillery Duel
 
-A [BabylonJS](http://babylonjs.com) game
+A [BabylonJS](http://babylonjs.com) game.
+
+Work in progress : http://jerome.bousquie.fr/BJS/dev/artillery/artillery.html  
+Blue cannon : arrow keys + ENTER
+Red cannon : X/C (left/right) + R/F (up/down) + V
--- level.js
+++ level.js
@@ -2,8 +2,16 @@
 "use strict";
 
 var ARTILLERY = ARTILLERY || {};
-ARTILLERY.scenes = ARTILLERY.scenes || {};
 
+// Artillery properties
+ARTILLERY.scenes = ARTILLERY.scenes || {};
+ARTILLERY.gravity = 1;
+ARTILLERY.controls = [
+    {up: false, down: false, left: false, right: false, fire: false}, 
+    {up: false, down: false, left: false, right: false, fire: false}
+];
+
+// Artillery functions
 ARTILLERY.generateLandscape = function(groundSize, sub, scene) {
     var groundTex = new BABYLON.Texture("images/ground.jpg", scene);
     groundTex.uScale = 30;
@@ -125,16 +133,55 @@ ARTILLERY.generateLandscape = function(groundSize, sub, scene) {
 
 ARTILLERY.generateCannon = function(id, size, color, position, angle, rotY,  scene) {
     var path = [new BABYLON.Vector3(0, 0, -size /2), new BABYLON.Vector3(0, 0, size / 2)];
-    var cannon = BABYLON.MeshBuilder.CreateTube("c"+id, {path: path, radius: size / 8, tessellation: 16, cap: BABYLON.Mesh._CAP_START}, scene);
+    var caliber = size / 8;
+    var cannon = BABYLON.MeshBuilder.CreateTube(id, {path: path, radius: caliber, tessellation: 16, cap: BABYLON.Mesh._CAP_START}, scene);
     cannon.position = position;
     cannon.rotation.x = angle;
     cannon.rotation.y = rotY;
-    var cannonMat = new BABYLON.StandardMaterial("cm"+id, scene);
+    var cannonMat = new BABYLON.StandardMaterial("mat-"+id, scene);
     cannonMat.diffuseColor = color;
     cannonMat.backFaceCulling = false;
     cannonMat.freeze();
     cannon.material = cannonMat;
+    // artillery properties
+    cannon.caliber = caliber;       // cannon caliber
+    cannon.capacity = 3;            // max load of bullets
+    cannon.nextBullet = 0;          // index of next (un-fired) bullet
+    cannon.bullets = [];            // bullet array
+    cannon.temperature = 0;         // cannon temperature
+    cannon.limitTemperature = 100;
     return cannon;
+};
+
+ARTILLERY.generateBullet = function(id, cannon, scene) {
+    var bullet = BABYLON.MeshBuilder.CreateSphere(id, {segments: 4, diameter: cannon.caliber}, scene);
+    bullet.material = cannon.material;
+    bullet.position.copyFrom(cannon.position);
+    // artillery properties
+    bullet.cannon = cannon;                 // reference to the cannon it belongs to
+    bullet.fired = false;                   // if the bullet is fired
+    bullet.warming = 50;                    // how much the bullets warms the cannon when fired
+    bullet.speed = 2;                     // bullet speed
+    bullet.dateFired = 0.0;                 // timestamp on fire
+    bullet.velocity = BABYLON.Vector3.Zero(); // initial velocity vector 
+    cannon.bullets.push(bullet);            // load the bullet into the cannon
+    return bullet;
+};
+
+
+ARTILLERY.bulletBalistic = function(bullet, ground) {
+    
+    var k = (Date.now() - bullet.dateFired) / 1000;
+
+    bullet.position.x = k * bullet.velocity.x + bullet.cannon.position.x;        //  x = vx * t + x0
+    bullet.position.z = k * bullet.velocity.z + bullet.cannon.position.z;        //  z = vz * t + z0
+    bullet.position.y = -k * k * ARTILLERY.gravity * 0.5 + k * bullet.velocity.y + bullet.cannon.position.y;     // y = -g * t² / 2 + vy * t + y0
+    if ( bullet.position.y <= ground.getHeightAtCoordinates(bullet.position.x, bullet.position.z) ) {
+        bullet.fired = false;
+        bullet.position.copyFrom(bullet.cannon.position);
+        bullet.cannon.nextBullet --;
+        bullet.cannon.nextBullet = (bullet.cannon.nextBullet < 0) ? 0 : bullet.cannon.nextBullet;
+    } 
 };
 
 ARTILLERY.bindControls = function(controls) {
@@ -169,7 +216,7 @@ ARTILLERY.bindControls = function(controls) {
         if (evt.keyCode === 67) { //C
             controls[1].right = true;
         }
-        if (evt.keyCode === 9) { //TAB
+        if (evt.keyCode === 86) { //V
             controls[1].fire = true;
         }
     });  
@@ -203,25 +250,23 @@ ARTILLERY.bindControls = function(controls) {
         if (evt.keyCode === 67) { //C
             controls[1].right = false;
         }
-        if (evt.keyCode === 9) { //TAB
+        if (evt.keyCode === 86) { //V
             controls[1].fire = false;
         }
     });
 };
 
-// control[0] == cannon1, controls[1] == cannon2
-ARTILLERY.controls = [
-    {up: false, down: false, left: false, right: false, fire: false}, 
-    {up: false, down: false, left: false, right: false, fire: false}
-    ];
 
+
+
+// Level logic
 ARTILLERY.scenes["level"] = function(canvas, engine) {
     // Scene and camera
     var scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3( .3, .5, .9);
     var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, -0), scene);
     camera.setPosition(new BABYLON.Vector3(0, 15, 30));
-    //camera.attachControl(canvas, true);
+    camera.attachControl(canvas, true);
 
     // Lights
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
@@ -244,24 +289,42 @@ ARTILLERY.scenes["level"] = function(canvas, engine) {
     z = groundSize / 6 * Math.random() + groundSize / 10 - groundSize / 2; 
     y = landscape.ground.getHeightAtCoordinates(x, z) + cannonSize / 2;
     var pos1 = new BABYLON.Vector3(x, y, z);
-    var cannon1 = ARTILLERY.generateCannon("1", cannonSize, BABYLON.Color3.Blue(), pos1, -Math.PI / 5, 0, scene);
+    var cannon1 = ARTILLERY.generateCannon("cannon1", cannonSize, BABYLON.Color3.Blue(), pos1, -Math.PI / 5, 0, scene);
    
     x = -groundSize / 6 * Math.random() - groundSize / 10 + groundSize / 2;
     z = -groundSize / 6 * Math.random() - groundSize / 10 + groundSize / 2; 
     y = landscape.ground.getHeightAtCoordinates(x, z) + cannonSize / 2;
     var pos2 = new BABYLON.Vector3(x, y, z);
-    var cannon2 = ARTILLERY.generateCannon("1", cannonSize, BABYLON.Color3.Red(), pos2, -Math.PI / 5, Math.PI, scene);  
+    var cannon2 = ARTILLERY.generateCannon("cannon2", cannonSize, BABYLON.Color3.Red(), pos2, -Math.PI / 5, Math.PI, scene);  
     
     var cannons = [cannon1, cannon2];  
+    
+    // Bullets
+    var ammoNb = 3;
+    var bullets = [];
+    var bNb = 0;
+    for (var c = 0; c < cannons.length; c++) {
+        var cannon = cannons[c];
+        for (var b = 0; b < ammoNb; b++) {
+            var bullet = ARTILLERY.generateBullet(bNb.toString, cannon, scene);
+            bullets.push(bullet);
+            bNb ++;
+        }
+    }
     
     ARTILLERY.bindControls(ARTILLERY.controls);
     
     var deltaX = 0.01;
     var deltaY = 0.01;
+    var coolingRate = 1;
     //scene.debugLayer.show();
     scene.registerBeforeRender(function() {
 
-        for (var c = 0; c < 2; c ++) {
+        // move cannons and fire
+        for (var c = 0; c < cannons.length; c ++) {
+            cannons[c].temperature -= coolingRate;
+            cannons[c].temperature = (cannons[c].temperature < 0) ? 0 : cannons[c].temperature; 
+            
             if (ARTILLERY.controls[c].up) {
                 cannons[c].rotation.x -= deltaX;
             } else if (ARTILLERY.controls[c].down) {
@@ -271,10 +334,32 @@ ARTILLERY.scenes["level"] = function(canvas, engine) {
                 cannons[c].rotation.y -= deltaY;
             } else if (ARTILLERY.controls[c].right) {
                 cannons[c].rotation.y += deltaY;
+            }
+            if (ARTILLERY.controls[c].fire && cannons[c].temperature <= cannons[c].limitTemperature) {
+                var loadedBullet = cannons[c].bullets[cannons[c].nextBullet];
+                if (loadedBullet) {     // if the cannon has still an avalaible bullet
+                    loadedBullet.fired = true;
+                    loadedBullet.dateFired = Date.now();
+                    var vx = Math.cos(cannons[c].rotation.y) * loadedBullet.speed;
+                    var vy = Math.sin(cannons[c].rotation.x) * loadedBullet.speed;
+                    var vx = Math.sin(cannons[c].rotation.y) * loadedBullet.speed;
+                    loadedBullet.velocity.copyFromFloats(x, y, z);
+                    cannons[c].temperature += loadedBullet.warming;
+                    cannons[c].nextBullet ++;
+                    cannons[c].nextBullet = (cannons[c].nextBullet < cannons[c].capacity) ? cannons[c].nextBullet : -1;
+                }
             }            
         }
         
-       camera.alpha += 0.001; 
+        // animate bullets
+        for (var b = 0; b < bullets.length; b++) {
+            var bullet = bullets[b];
+            if (bullet.fired) {
+                ARTILLERY.bulletBalistic(bullet, landscape.ground);
+            }
+        }
+        
+       //camera.alpha += 0.001; 
     });
     
     return scene;
